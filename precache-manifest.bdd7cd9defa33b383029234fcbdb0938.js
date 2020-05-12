a0fcb880905bebafe8437f096e955b66bdbe040c
Cherise Knox <cheriseknox@gmail.com>
2020-05-11 21:02:45 -0400

    Updates

---
+++ precache-manifest.bdd7cd9defa33b383029234fcbdb0938.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d6e8b08ad0545e0eeb19e4fcd99f3d",
+    "url": "/cherisekportfolio/index.html"
+  },
+  {
+    "revision": "14040083b8171fcfd5d9",
+    "url": "/cherisekportfolio/static/css/main.3754b271.chunk.css"
+  },
+  {
+    "revision": "c4c91fde33b17d87c59b",
+    "url": "/cherisekportfolio/static/js/2.868fcade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cherisekportfolio/static/js/2.868fcade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14040083b8171fcfd5d9",
+    "url": "/cherisekportfolio/static/js/main.e56bef91.chunk.js"
+  },
+  {
+    "revision": "e544bab7de7b15909275",
+    "url": "/cherisekportfolio/static/js/runtime-main.1cc76ff9.js"
+  },
+  {
+    "revision": "3e5b5b0165be780b49b62ab7a25f869b",
+    "url": "/cherisekportfolio/static/media/iab.3e5b5b01.png"
+  },
+  {
+    "revision": "2965802153cdb4389ae87f1884c6bac5",
+    "url": "/cherisekportfolio/static/media/movie.29658021.png"
+  },
+  {
+    "revision": "371572ca9ca14ca9f9e8d08bc6617b88",
+    "url": "/cherisekportfolio/static/media/profile-pic.371572ca.jpg"
+  },
+  {
+    "revision": "e414b30a5ff189798f081e2abc1152bc",
+    "url": "/cherisekportfolio/static/media/verity.e414b30a.png"
+  }
+]);
